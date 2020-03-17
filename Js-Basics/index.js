dd5ed549f441d12a732465ff4b3b90a8ee6afdf5
alireza <arn1363@yahoo.com>
2020-03-17 23:24:57 +0330

    another commit

--- Js-Basics/index.js
+++ Js-Basics/index.js
@@ -108,3 +108,20 @@
 //   return result;
 // }
 //-----------------------------------------------------------------------
+
+/** Prime Numbers */
+showPrimes(20);
+
+function showPrimes(limit) {
+  for (let number = 2; number <= limit; number++) {
+    let isPrime = true;
+
+    for (let factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) console.log(number);
+  }
+}
